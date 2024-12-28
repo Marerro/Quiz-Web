06ae5cbed2f2b86eebdd4f4fcb19edad247c4ebf
Marerro <kuneckisj@gmail.com>
2024-12-28 16:17:22 +0200

    Added UI features in Question-Page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,126 +1,123 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router";
-import { getAll } from "../helpers/get"
+import { getAll } from "../helpers/get";
 
 function Home() {
-    const [questions, setQuestions] = useState([]);
-    const [selectedCategories, setSelectedCategories] = useState("");
-    const [difficulty, setDifficulty] = useState("");
-    const [filteredQuestions, setFilteredQuestions] = useState([]);
+  const [questions, setQuestions] = useState([]);
+  const [selectedCategories, setSelectedCategories] = useState("");
+  const [difficulty, setDifficulty] = useState("");
+  const [filteredQuestions, setFilteredQuestions] = useState([]);
 
-    // Function makes answers from object to array and add true answer information
-    const AnswersFromObject = (question) => {
-        const answersArray = [];
-        for (const key in question.answers) {
-            if (question.answers[key] !== null) {
-                answersArray.push({
-                    id:key,
-                    answer: question.answers[key],
-                    isCorrect: question.correct_answers[`${key}_correct`] === "true",
-                });
-            }
-        }
-        return answersArray;
+  // Function makes answers from object to array and add true answer information
+  const AnswersFromObject = (question) => {
+    const answersArray = [];
+    for (const key in question.answers) {
+      if (question.answers[key] !== null) {
+        answersArray.push({
+          id: key,
+          answer: question.answers[key],
+          isCorrect: question.correct_answers[`${key}_correct`] === "true",
+        });
+      }
     }
+    return answersArray;
+  };
 
-    // Get all questions
-    const getAllQuestions = async () => {
-        const response = await getAll(); 
-        const questionsWithAnswers = response.map((question) => ({
-            ...question,
-            answersArray: AnswersFromObject(question),
-        }))
-        setQuestions(questionsWithAnswers);
-    };
+  // Get all questions
+  const getAllQuestions = async () => {
+    const response = await getAll();
+    const questionsWithAnswers = response.map((question) => ({
+      ...question,
+      answersArray: AnswersFromObject(question),
+    }));
+    setQuestions(questionsWithAnswers);
+  };
 
-    console.log(questions)
+  // Filter by what user select
+  const checkSelect = () => {
+    if (selectedCategories && difficulty) {
+      const filtered = questions.filter(
+        (question) =>
+          question.category.trim().toLowerCase() ===
+            selectedCategories.trim().toLowerCase() &&
+          question.difficulty.trim().toLowerCase() === difficulty.trim().toLowerCase()
+      );
+      setFilteredQuestions(filtered);
+    }
+  };
 
-    // Filter by what user select
-    const checkSelect = () => {
-        if (selectedCategories && difficulty) {
-            const filtered = questions.filter(
-                (question) =>
-                    question.category === selectedCategories &&
-                    question.difficulty === difficulty
-            );
-            setFilteredQuestions(filtered);
-        }
-    };
+  useEffect(() => {
+    getAllQuestions();
+  }, []);
 
-    
-    useEffect(() => {
-        getAllQuestions();
-    }, []);
+  useEffect(() => {
+    checkSelect();
+  }, [selectedCategories, difficulty, questions]);
 
-    
-    useEffect(() => {
-        checkSelect();
-    }, [selectedCategories, difficulty, questions]);
+  return (
+    <div>
+      <div>
+        <h1 className="text-[#293264] Karla font-bold leading-normal text-center">
+          Quizzical
+        </h1>
+        <p className="Inter text-[#293264] text-center not-italic font-normal leading-normal">
+          Some description if needed
+        </p>
+      </div>
 
-    return (
-        <div>
-            <div>
-                <h1 className="text-[#293264] Karla font-bold leading-normal text-center">
-                    Quizzical
-                </h1>
-                <p className="Inter text-[#293264] text-center not-italic font-normal leading-normal">
-                    Some description if needed
-                </p>
-            </div>
+      {/* Difficulty */}
+      <div className="mb-5">
+        <form className="max-w-sm mx-auto">
+          <select
+            id="difficulty"
+            onChange={(e) => setDifficulty(e.target.value)}
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+          >
+            <option value="">Select Difficulty</option>
+            {[...new Set(questions.map((q) => q.difficulty || "Unknown"))].map(
+              (difficulty, index) => (
+                <option key={index} value={difficulty}>
+                  {difficulty}
+                </option>
+              )
+            )}
+          </select>
+        </form>
+      </div>
 
-            {/* difficulty */}
-            <div className="mb-5">
-                <form className="max-w-sm mx-auto">
-                    <select
-                        id="difficulty"
-                        onChange={(e) => setDifficulty(e.target.value)}
-                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                    >
-                        <option value="">Difficulty</option>
-                        {[...new Set(questions.map((q) => q.difficulty))].map(
-                            (difficulty, index) => (
-                                <option key={index} value={difficulty}>
-                                    {difficulty}
-                                </option>
-                            )
-                        )}
-                    </select>
-                </form>
-            </div>
+      {/* Category */}
+      <div className="mb-5">
+        <form className="max-w-sm mx-auto">
+          <select
+            id="categories"
+            onChange={(e) => setSelectedCategories(e.target.value)}
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+          >
+            <option value="">Select Category</option>
+            {[
+              ...new Set(questions.map((q) => q.category || "Uncategorized")),
+            ].map((category, index) => (
+              <option key={index} value={category}>
+                {category}
+              </option>
+            ))}
+          </select>
+        </form>
+      </div>
 
-            {/* Category */}
-            <div className="mb-5">
-                <form className="max-w-sm mx-auto">
-                    <select
-                        id="categories"
-                        onChange={(e) => setSelectedCategories(e.target.value)}
-                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                    >
-                        <option value="">Category</option>
-                        {[...new Set(questions.map((q) => q.category))].map(
-                            (category, index) => (
-                                <option key={index} value={category}>
-                                    {category}
-                                </option>
-                            )
-                        )}
-                    </select>
-                </form>
-            </div>
-
-            {/* Questions when press "Start Quiz" */}
-            <div className="text-center">
-                <Link to="/question" state={{ questions: filteredQuestions }}>
-                    <button
-                        type="button"
-                        className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2"
-                    >
-                        Start Quiz
-                    </button>
-                </Link>
-            </div>
-        </div>
-    );
+      {/* Questions when press "Start Quiz" */}
+      <div className="text-center">
+        <Link to="/question" state={{ questions: filteredQuestions }}>
+          <button
+            type="button"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2"
+          >
+            Start Quiz
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,13 +1,21 @@
 import { useLocation } from "react-router";
+import { useState } from "react";
 
 function Question() {
     const location = useLocation();
     const { questions } = location.state || { questions: [] }; // Get filtered questions by useLocation
+    const [clicked, setClicked] = useState({});
 
-    console.log(questions);
+    const clickHandler = (e, answerId, questionId) => {
+            setClicked((prev) => ({
+                ...prev,
+                [questionId]: answerId,
+            }))
+    }
+
     return (
         <div className="w-[34.375rem] ml-[4.69rem] p-0">
-            {questions.length > 0 &&
+            {
                 questions.map((question, index) => (
                     <div key={index} className="mb-8">
                         <h3 className="text-[#293264] ml-[4.69rem] mb-5 mt-[5rem] text-[1rem] font-[700] Karla">
@@ -15,13 +23,21 @@ function Question() {
                         </h3>
 
                         {question.answersArray && (
-                            <ul>
+                            <ul className="flex ml-[4.69rem] gap-5">
                                 {question.answersArray.map((answer) => (
-                                    <li key={answer.id}>
+                                    <li
+                                    key={answer.id}
+                                    >
                                         <button
                                             type="button"
+                                            className={`rounded-[2rem] border-[#4D5B9E] border-[1px] w-[160.922px] h-[39.65px] ${clicked[question.id] === answer.id
+                                            ? "bg-[#D6DBF5]"
+                                            : "bg-[#F5F7FB]"
+                                            } flex items-center justify-center text-center break-words px-2`}
+                                            onClick={(e) => clickHandler(e, answer.id, question.id) }
                                         >
-                                            {answer.answer}
+                                            <span className="Inter text-[10.24px] font-[400] leading-normal">{answer.answer}</span>
+                                           
                                         </button>
                                     </li>
                                 ))}
